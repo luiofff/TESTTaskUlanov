781c729275bcc6a22117108809051d0a1c4d1e89
luiofff <95875204+luiofff@users.noreply.github.com>
2023-10-29 16:47:09 +0300

    add backend

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -6,20 +6,20 @@ import styles from "./Card.module.css"
 import phone_ico from "./Assets/phone_ico.svg"
 import email_ico from "./Assets/email_ico.svg"
 
-const Card = () => {
+const Card = ({ name, phone, email }) => {
 
     return (
         <>
            <div className={styles.card}>
-                <h2>Евгения Савченко</h2>
+                <h2>{name}</h2>
                 <div className={styles.infornation_block}>
                     <div className={styles.content_block}>
                         <img src={phone_ico} />
-                        <span className={styles.info_text}>+7 (918) 078-17-05</span>
+                        <span className={styles.info_text}>{phone}</span>
                     </div>
                     <div className={styles.content_block}>
                         <img src={email_ico} />
-                        <span className={styles.info_text}>yysavchenk@mail.ru</span>
+                        <span className={styles.info_text}>{email}</span>
                     </div>
                 </div>
            </div>
--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -10,51 +10,130 @@ import Card from "../Card/Card";
 import Search_ico from "./Assets/Search_ico.svg"
 import Close_ico from "./Assets/Close_ico.svg"
 
+
+
+
 const MainPage = () => {
 
     const [open, setOpen] = React.useState(false);
+    const [users, setUsers] = React.useState([]);
+    const [searchTerm, setSearchTerm] = React.useState('');
+    const [userInfo, setUserInfo] = React.useState([])
+
+    const activeCard = (user) => {
+        setOpen(true)
+        setUserInfo(user);
+        console.log(userInfo)
+    }
+
+    // Function to fetch user data from the server
+    const fetchUsers = async () => {
+        try {
+        const response = await fetch(`http://localhost:3001/?term=${searchTerm}`);
+        const data = await response.json();
+        setUsers(data);
+        } catch (error) {
+        console.error('Error fetching user data:', error);
+        }
+    };
+
+    // Load user data initially
+    React.useEffect(() => {
+        fetchUsers();
+    }, [searchTerm]);
 
     return (
         <>
-            <section className={styles.content}>
+            <div className={styles.content}>
                 <div className={styles.search_block}>
-                    <input type="text" />
-                    <img src={Search_ico} alt="" />
+                    <input
+                        type="text"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className={styles.input}
+                    />
+                    <img src={Search_ico} onClick={fetchUsers} />
                 </div>
                 <div className={styles.card_space}>
-                    <Button onClick={() => setOpen(true)}>
-                        <Card />
-                    </Button>
-                    
+                    {users.map((user, index) => (
+                        <Button className={styles.card} key={index} onClick={() => activeCard([user.name, user.phone, user.email, user.hire_date, user.position_name, user.department])}>
+                            <Card 
+                                name={user.name}
+                                phone={user.phone}
+                                email={user.email}
+                            />
+                        </Button>
+                    ))}
 
                 </div>
-            </section>
+            </div>
             <Modal open={open} onClose={() => setOpen(false)}>
                 <div className={styles.modal_background}>
                     
                 </div>
                 <div className={styles.modal}>
                     <div className={styles.top_bar_modal}>
-                        <h2>Евгения Савченко</h2>
+                        <h2>{userInfo[0]}</h2>
                         <Button onClick={() => setOpen(false)}>
                             <img src={Close_ico} />
                         </Button>
                     </div>
                     <div className={styles.main_info_block}>
-                        <div className={styles.text_content}>
-                            <span className={styles.fixed_txt}>Телефон:</span>
-                            <span className={styles.fixed_txt}>Почта:</span>
-                            <span className={styles.fixed_txt}>Дата приема:</span>
-                            <span className={styles.fixed_txt}>Должность:</span>
-                            <span className={styles.fixed_txt}>Подразделение:</span>
+                        <div className={styles.text_block}>
+                            <div className={styles.fix_text}>
+                                <span className={styles.fixed_txt}>Телефон:</span>
+                            </div>
+                            <div className={styles.change_text}>
+                                <div className={styles.changeabled_txt}>
+                                    {userInfo[1]}
+                                </div>
+                            </div>
                         </div>
-                        <div className={styles.text_content}>
-                            <span className={styles.changeabled_txt}>+7 (918) 078-17-05</span>
-                            <span className={styles.changeabled_txt}>yysavch1@mts.ru</span>
-                            <span className={styles.changeabled_txt}>15.10.2020</span>
-                            <span className={styles.changeabled_txt}>Дизайнер</span>
-                            <span className={styles.changeabled_txt}>Трайб автоматизированных систем контактных центров</span>
+
+                        <div className={styles.text_block}>
+                            <div className={styles.fix_text}>
+                                <span className={styles.fixed_txt}>Почта:</span>
+                            </div>
+                            <div className={styles.change_text}>
+                                <div className={styles.changeabled_txt}>
+                                    {userInfo[2]}
+                                </div>
+                            </div>
                         </div>
+
+                        <div className={styles.text_block}>
+                            <div className={styles.fix_text}>
+                                <span className={styles.fixed_txt}>Дата приема:</span>
+                            </div>
+                            <div className={styles.change_text}>
+                                <div className={styles.changeabled_txt}>
+                                    {userInfo[3]}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={styles.text_block}>
+                            <div className={styles.fix_text}>
+                                <span className={styles.fixed_txt}>Должность:</span>
+                            </div>
+                            <div className={styles.change_text}>
+                                <div className={styles.changeabled_txt}>
+                                    {userInfo[4]}
+                                </div>
+                            </div>
+                        </div>
+                       
+                        <div className={styles.text_block}>
+                            <div className={styles.fix_text}>
+                                <span className={styles.fixed_txt}>Подразделение:</span>
+                            </div>
+                            <div className={styles.change_text}>
+                                <div className={styles.changeabled_txt}>
+                                    {userInfo[5]}
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                     <div className={styles.additional_info_block}>
                         <span className={styles.title_text_additional}>Дополнительная информация:</span>
